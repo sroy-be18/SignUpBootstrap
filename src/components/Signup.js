fba9705483043a59a21b181a1a5684a0a9470305
sroy-be18 <54741064+sroy-be18@users.noreply.github.com>
2022-10-19 15:58:42 +0530

    2 files modified

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -44,8 +44,8 @@ export const Signup = () => {
             <span className="col-1 line"></span>
             <span className="col-auto sub-title">Sign Up With</span>
           </div>
-          <div className="row signup-options">
-            <div className="col-6 signup-option">
+          <div className="row justify-content-end signup-options">
+            <div className="col-md-6 signup-option">
               <button className="signup-options-btn">
                 <img
                   className="option-img"
@@ -55,7 +55,7 @@ export const Signup = () => {
                 <div className="option-title">Sign up with Google</div>
               </button>
             </div>
-            <div className="col-6 signup-option">
+            <div className="col-md-6 signup-option">
               <button className="signup-options-btn">
                 <img
                   className="option-img"
